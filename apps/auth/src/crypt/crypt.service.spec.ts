5a7e12177774ac38119739e68caa0a8956fe5cb7
Yauheni Yafimenka <yafimenka.jk@gmail.com>
2021-10-21 11:20:07 +0300

    feat: add unit-test of crypt.service

--- apps/auth/src/crypt/crypt.service.spec.ts
+++ apps/auth/src/crypt/crypt.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import CryptService from './crypt.service';
+import { CRYPT_MODULE_OPTIONS } from './crypt.constants';
+import { Provider } from '@nestjs/common';
 
 describe('CryptService', () => {
   let service: CryptService;
 
   beforeEach(async () => {
+    const optionsProvider: Provider = {
+      provide: CRYPT_MODULE_OPTIONS,
+      useValue: {
+        saltOrRounds: 10,
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CryptService],
+      providers: [CryptService, optionsProvider],
     }).compile();
 
     service = module.get<CryptService>(CryptService);
@@ -14,5 +23,24 @@ describe('CryptService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create hash and compare - success', async () => {
+    const password = 'asdglasdv;lkjwe2134t';
+
+    const hash = await service.getHash(password);
+    const compareResult = await service.compare(password, hash);
+
+    expect(compareResult).toBeTruthy();
+  });
+
+  it('create hash and compare with wrong password - fail', async () => {
+    const password = 'asdglasdv;lkjwe2134t';
+    const password2 = 'jhklasfdghjklafddfgg4';
+
+    const hash = await service.getHash(password);
+    const compareResult = await service.compare(password2, hash);
+
+    expect(compareResult).toBeFalsy();
   });
 });
